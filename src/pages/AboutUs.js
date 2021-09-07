310ce641ea0b92a6b802e613788ac74565801bae
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-09-07 14:05:55 +0100

    added Services component

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import React from "react";
 import About from "../components/About";
+import Services from "../components/Services";
 
 const AboutUs = () => {
   return (
     <div>
       <About />
+      <Services />
     </div>
   );
 };
