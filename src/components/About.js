6f2ae83a6f61033a052e796ef149958172c00980
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-09-07 17:07:26 +0100

    added global styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 import home1 from "../img/home1.png";
-import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
-const About = () => {
+const AboutSection = () => {
   return (
-    <Abouts>
+    <About>
       <Description>
         <div className="title">
           <Hide>
@@ -26,39 +26,8 @@ const About = () => {
       <Image>
         <img src={home1} alt="camera" />
       </Image>
-    </Abouts>
+    </About>
   );
 };
 
-const Abouts = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: #fff;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
-
-export default About;
+export default AboutSection;
