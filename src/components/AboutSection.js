b7a175026310ea9ff9c69a5592e18d798d6d5436
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-09-08 13:48:06 +0100

    added animation to tile on homepage

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,25 +1,48 @@
 import React from "react";
 import home from "../img/home.jpg";
 import { About, Description, Image, Hide } from "../styles";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+        staggerChildren: 1,
+        when: "beforeChildren",
+      },
+    },
+  };
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>Come with me</h2>
+            <motion.h2 variants={titleAnim}>Come with me</motion.h2>
           </Hide>
           <Hide>
-            <h2>to a world of </h2>
+            <motion.h2 variants={titleAnim}>to a world of </motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               {" "}
               <span>imagination</span>
-            </h2>
+            </motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>Get In Contact for any of your photography needs</p>
         <button>Contact</button>
       </Description>
